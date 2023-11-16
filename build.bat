03cc79b361bd813d6817a5efb40f9f5e2002ab40
chaihahaha <chai836275709@gmail.com>
2023-11-16 21:46:42 +0800

    add CMake build script, fix python test script

---
+++ build.bat
@@ -0,0 +1,11 @@
+@echo off
+set VS_DEV_CMD="C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+set ARCH="amd64"
+call %VS_DEV_CMD% -arch=%ARCH%
+cd /d %~dp0
+mkdir build
+cd build
+cmake ..
+cmake --build . --config Release
+xcopy Release\* ..\
+pause
--- test_sp.py
+++ test_sp.py
@@ -1,26 +1,17 @@
 import subprocess
 
-mt5_proc = subprocess.Popen("OrtMT5.exe", stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
+ort_mt5_path = "ortmt5.exe"
+model_path = "mt5-ja_zh_beam_search.onnx"
+max_length_int = 128
+min_length_int = 1
+num_beams_int = 8
+num_return_sequences_int = 1
+length_penalty_float = 1.1
+repetition_penalty_float = 1.1
 
-in_str = '''17
-1042
-264
-462
-338
-12001
-25134
-31150
-70694
-10978
-67916
-81634
-574
-86977
-56157
-66637
-309
-1
-'''
+mt5_proc = subprocess.Popen([ort_mt5_path, model_path, str(max_length_int), str(min_length_int), str(num_beams_int), str(num_return_sequences_int), str(length_penalty_float), str(repetition_penalty_float)], stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
+
+in_str = '17 1042 264 462 338 12001 25134 31150 70694 10978 67916 81634 574 86977 56157 66637 309 1\n'
 mt5_proc.stdin.write(in_str)
 od1 = mt5_proc.stdout.readline()
 print(od1)
