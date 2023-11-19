d2308150b92e390a0d2cccc79527ec64a2a8ed41
chaihahaha <chai836275709@gmail.com>
2023-11-19 15:37:10 +0800

    better test script

--- test_sp.py
+++ test_sp.py
@@ -1,24 +1,55 @@
 import subprocess
 import time
+import tokenizers
+import argparse
 
-ort_mt5_path = "ortmt5.exe"
-model_path = r"mt5-ja_zh_beam_search.onnx"
-max_length_int = 128
-min_length_int = 1
-num_beams_int = 8
-num_return_sequences_int = 1
-length_penalty_float = 1.1
-repetition_penalty_float = 1.1
+def parse_args():
+    parser = argparse.ArgumentParser(description='ortmt5 test script')
+    parser.add_argument('-m', '--onnx_model_path', default='mt5-ja_zh_beam_search.onnx', type=str, help='the path to mT5 onnx model')
+    parser.add_argument('-t', '--tokenizer_path', default='tokenizer.json', type=str, help='the path to mT5 tokenizer model')
+    parser.add_argument('-e', '--ortmt5_executable_path', default='ortmt5.exe', type=str, help='the path to ortmt5.exe')
+    parser.add_argument('-max', '--max_length', default=128, type=int, help='max generation length')
+    parser.add_argument('-min', '--min_length', default=1, type=int, help='min generation length')
+    parser.add_argument('-nb', '--n_beams', default=8, type=int, help='number of beams to search')
+    parser.add_argument('-ns', '--n_sequences', default=1, type=int, help='number of output sequences')
+    parser.add_argument('-lp', '--length_penalty', default=1.3, type=float, help='penalty of generating long text')
+    parser.add_argument('-rp', '--repetition_penalty', default=1.3, type=float, help='penalty of generating repeating text')
+    args = parser.parse_args()
+    return args
 
-mt5_proc = subprocess.Popen([ort_mt5_path, model_path, str(max_length_int), str(min_length_int), str(num_beams_int), str(num_return_sequences_int), str(length_penalty_float), str(repetition_penalty_float)], stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
+def translate_user_input(mt5_proc, tok):
+    in_str = input("Setence to be translated: ")
+    input_encoding = tok.encode('<-ja2zh-> ' + in_str)
+    input_ids = input_encoding.ids
+    pipe_in_str = " ".join([str(len(input_ids))] + [str(i) for i in input_ids] + ['\n'])
+    print("mt5 subprocess stdin:", pipe_in_str)
 
-in_str = '17 1042 264 462 338 12001 25134 31150 70694 10978 67916 81634 574 86977 56157 66637 309 1\n'
-tik = time.time()
-mt5_proc.stdin.write(in_str)
-od1 = mt5_proc.stdout.readline()
-print(od1)
-mt5_proc.stdin.write(in_str)
-od2 = mt5_proc.stdout.readline()
-print(od2)
-print("time elapsed:", time.time() - tik)
-mt5_proc.communicate("exit")
+    tik = time.time()
+    mt5_proc.stdin.write(pipe_in_str)
+    pipe_out_str = mt5_proc.stdout.readline()
+    print("time elapsed:", time.time() - tik)
+    output_ids = [int(i) for i in pipe_out_str.split()]
+    print(tok.decode(output_ids))
+    return
+
+if __name__=='__main__':
+    args = parse_args()
+
+    ort_mt5_path = args.ortmt5_executable_path
+    model_path = args.onnx_model_path
+    tok_path = args.tokenizer_path
+    max_length_int = args.max_length
+    min_length_int = args.min_length
+    num_beams_int = args.n_beams
+    num_return_sequences_int = args.n_sequences
+    length_penalty_float = args.length_penalty
+    repetition_penalty_float = args.repetition_penalty
+    
+    mt5_args = [ort_mt5_path, model_path, str(max_length_int), str(min_length_int), str(num_beams_int), str(num_return_sequences_int), str(length_penalty_float), str(repetition_penalty_float)]
+    print("mt5 subprocess args:", mt5_args)
+    mt5_proc = subprocess.Popen(mt5_args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
+    
+    tok = tokenizers.Tokenizer.from_file(tok_path)
+
+    while True:
+        translate_user_input(mt5_proc, tok)
