d960db7fba98cf14ca7e5f462cbe07946e517163
chaihahaha <chai836275709@gmail.com>
2023-11-23 22:03:53 +0800

    fix arync fail by circular array

--- OrtMT5.cpp
+++ OrtMT5.cpp
@@ -2,12 +2,23 @@
 
 #pragma comment(lib, "onnxruntime.lib")
 
+#define MAX_ASYNC_RUNS 6000
+
 //static std::atomic_int thread_cnt{0};
 static std::atomic_bool atomic_printing{false};
 std::unique_ptr<Ort::Session> session = nullptr;
 std::unique_ptr<Ort::Env> env = nullptr;
-static size_t num_input_nodes = -1;
+static size_t num_input_nodes = 0;
 static int max_threads = -1;
+
+void clear_ortvalue_vector(OrtValue** a, size_t len)
+{
+    for (size_t i = 0; i < len; i++)
+    {
+        Ort::Value v = Ort::Value(a[i]);
+        v.release();
+    }
+}
 
 void print_tensor(Ort::Value& tensor)
 {
@@ -37,7 +48,9 @@ void AsyncCallback(void* user_data, OrtValue** outputs, size_t num_outputs, OrtS
     Ort::Status status(status_ptr);
     Ort::Value output_value(outputs[0]);
     if (status.IsOK())
+    {
         print_tensor(output_value);
+    }
     else
     {
 #ifdef DEBUG
@@ -87,9 +100,9 @@ int wmain(int argc, wchar_t* argv[])
 #ifdef DEBUG
     std::cout << "max threads:" << max_threads << std::endl;
 #endif
-    //session_options.SetIntraOpNumThreads(max_threads/2);
-    //session_options.SetExecutionMode(ExecutionMode::ORT_PARALLEL);
-    session_options.SetInterOpNumThreads(max_threads);
+    session_options.SetIntraOpNumThreads(max_threads/2);
+    session_options.SetExecutionMode(ExecutionMode::ORT_PARALLEL);
+    session_options.SetInterOpNumThreads(max_threads/2);
     session_options.SetGraphOptimizationLevel(
         GraphOptimizationLevel::ORT_ENABLE_ALL);
     //session_options.DisablePerSessionThreads();
@@ -155,10 +168,10 @@ int wmain(int argc, wchar_t* argv[])
     output_names.resize(1);
     std::vector<int64_t> output_ids_dims{1, 1, max_length_int};
 
-    std::vector<std::vector<int32_t>> input_ids_pool;
-    std::vector<std::vector<int64_t>> input_ids_dims_pool;
-    std::vector<std::vector<Ort::Value>> input_tensors_pool;
-    std::vector<std::vector<Ort::Value>> output_tensors_pool;
+    std::array<std::vector<int32_t>, MAX_ASYNC_RUNS> input_ids_pool;
+    std::array<std::vector<int64_t>, MAX_ASYNC_RUNS> input_ids_dims_pool;
+    std::array<std::vector<Ort::Value>, MAX_ASYNC_RUNS> input_tensors_pool;
+    std::array<std::vector<Ort::Value>, MAX_ASYNC_RUNS> output_tensors_pool;
 
     int32_t input_length = -1;
     int32_t input_value = -1;
@@ -198,9 +211,12 @@ int wmain(int argc, wchar_t* argv[])
         Ort::Value repetition_penalty = Ort::Value::CreateTensor<float>(memory_info, repetition_penalty_values.data(), repetition_penalty_values.size(),
             repetition_penalty_dims.data(), repetition_penalty_dims.size());
 
-        input_ids_pool.push_back(std::move(py_input_ids));
+        input_ids_pool[scnt].clear();
+        input_ids_pool[scnt] = std::move(py_input_ids);
+
         std::vector<int64_t> input_ids_dims{1, (int64_t)input_ids_pool[scnt].size()};
-        input_ids_dims_pool.push_back(std::move(input_ids_dims));
+        input_ids_dims_pool[scnt].clear();
+        input_ids_dims_pool[scnt] = std::move(input_ids_dims);
 
         Ort::Value input_ids = Ort::Value::CreateTensor<int32_t>(memory_info, input_ids_pool[scnt].data(), input_ids_pool[scnt].size(),
             input_ids_dims_pool[scnt].data(), input_ids_dims_pool[scnt].size());
@@ -213,7 +229,9 @@ int wmain(int argc, wchar_t* argv[])
         input_tensors.push_back(std::move(num_return_sequences));
         input_tensors.push_back(std::move(length_penalty));
         input_tensors.push_back(std::move(repetition_penalty));
-        input_tensors_pool.push_back(std::move(input_tensors));
+        clear_ortvalue_vector(reinterpret_cast<OrtValue**>(input_tensors_pool[scnt].data()), input_tensors_pool[scnt].size());
+        input_tensors_pool[scnt].clear();
+        input_tensors_pool[scnt] = std::move(input_tensors);
 
         std::vector<Ort::Value> output_tensors;
 
@@ -224,7 +242,9 @@ int wmain(int argc, wchar_t* argv[])
                     output_ids_dims.size()
                 )
         );
-        output_tensors_pool.push_back(std::move(output_tensors));
+        clear_ortvalue_vector(reinterpret_cast<OrtValue**>(output_tensors_pool[scnt].data()), output_tensors_pool[scnt].size());
+        output_tensors_pool[scnt].clear();
+        output_tensors_pool[scnt] = std::move(output_tensors);
 
         session->RunAsync(
             Ort::RunOptions{nullptr},
@@ -237,13 +257,7 @@ int wmain(int argc, wchar_t* argv[])
             AsyncCallback,
             nullptr
             );
-        std::chrono::duration<double, std::milli> dur{100};
-        std::this_thread::sleep_for(dur);
-        //for (int i = 0; i < input_tensors_pool[scnt].size(); i++)
-        //{
-        //    input_tensors_pool[scnt][i].release();
-        //}
-        scnt += 1;
+        scnt = (scnt + 1) % MAX_ASYNC_RUNS;
     }
 #ifdef DEBUG
     std::cout << "debug on" << std::endl;
